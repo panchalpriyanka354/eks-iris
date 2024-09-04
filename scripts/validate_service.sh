5c09a1ba98a7f97feffba1cfe63753bff5ee2911
panchalpriyanka354 <panchalpriyanka354@gmail.com>
2024-09-04 18:35:40 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "Validating the service..."
+
+# Wait for a few seconds to allow the app to start
+sleep 10
+
+# Validate that the application is running
+curl -f http://localhost:8080 || {
+  echo "Validation failed. The application is not running correctly."
+  exit 1
+}
+
+echo "Validation succeeded. The application is running correctly."
