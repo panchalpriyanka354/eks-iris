e44b1e99b7ac03d3908519fedd364b87e2873426
panchalpriyanka354 <panchalpriyanka354@gmail.com>
2024-09-04 14:59:11 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
 	# Stop any existing containers
-	sudo docker stop $(docker ps -a -q) || true
-	sudo docker rm $(docker ps -a -q) || true
-
-	# Pull the latest Docker image from ECR
-	sudo docker pull ecr-iris-repo:latest
-
-	# Run the Docker container
-	sudo docker run -d -p 8080:5000 ecr-iris-repo:latest
+	
