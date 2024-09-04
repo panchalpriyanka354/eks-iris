4cbbd47bca48880b83332b6d1d0184de998e186d
panchalpriyanka354 <panchalpriyanka354@gmail.com>
2024-09-04 13:27:10 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -9,14 +9,14 @@ export AWS_DEFAULT_REGION=us-east-1
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 545009869835.dkr.ecr.us-east-1.amazonaws.com
 
 # Step 3: Pull the latest Docker image from ECR
-docker pull 545009869835.dkr.ecr.us-east-1.amazonaws.com/ecr-iris-repo:latest
+sudo docker pull 545009869835.dkr.ecr.us-east-1.amazonaws.com/ecr-iris-repo:latest
 
 # Step 4: Stop and remove any existing container with the same name
-docker stop ml-iris-app || true
-docker rm ml-iris-app || true
+sudo docker stop ml-iris-app || true
+sudo docker rm ml-iris-app || true
 
 # Step 5: Run the Docker container with the latest image
-docker run -d -p 8080:5000 --name ml-iris-app \
+sudo docker run -d -p 8080:5000 --name ml-iris-app \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   -e AWS_DEFAULT_REGION=us-east-1 \
