e3d79002c7e72b72830fd5dd0e248c8c8b476d36
panchalpriyanka354 <panchalpriyanka354@gmail.com>
2025-03-01 15:13:24 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Step 1: Export AWS Credentials (use this cautiously; IAM roles are preferred)
-export AWS_ACCESS_KEY_ID=AKIAX5ZI6UQFQA44MQWM
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=AKIA4SDNVOT3P6OBZPV5
+export AWS_SECRET_ACCESS_KEY=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
 export AWS_DEFAULT_REGION=us-east-1
 
 # Step 2: Log in to Amazon ECR
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 545009869835.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 863518422262.dkr.ecr.us-east-1.amazonaws.com
 
 # Step 3: Pull the latest Docker image from ECR
-sudo docker pull 545009869835.dkr.ecr.us-east-1.amazonaws.com/ecr-iris-repo:latest
+sudo docker pull 863518422262.dkr.ecr.us-east-1.amazonaws.com/ecr-iris-repo
 
 # Step 4: Stop and remove any existing container with the same name
 sudo docker stop ml-iris-app || true
@@ -20,6 +20,6 @@ sudo docker run -d -p 8080:5000 --name ml-iris-app \
   -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
   -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
   -e AWS_DEFAULT_REGION=us-east-1 \
-  545009869835.dkr.ecr.us-east-1.amazonaws.com/ecr-iris-repo:latest
+  863518422262.dkr.ecr.us-east-1.amazonaws.com/ecr-iris-repo
 
 echo "Docker container ml-iris-app deployed successfully on Dev/QA environment."
