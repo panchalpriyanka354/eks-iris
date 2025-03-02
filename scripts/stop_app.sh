9248e0b4e3c68847626dd7afff5207e464efd109
panchalpriyanka354 <panchalpriyanka354@gmail.com>
2025-03-02 16:34:11 +0530

    Update stop_app.sh

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-# Stop the Docker container if it's running
-sudo docker stop ml-iris-app || true
-
-# Remove the stopped container
-sudo docker rm ml-iris-app || true
-
-echo "Docker container ml-iris-app stopped and removed successfully."
+echo "ApplicationStart hook executed successfully!"
+exit 0
